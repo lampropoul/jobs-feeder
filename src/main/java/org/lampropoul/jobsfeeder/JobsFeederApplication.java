c9d0cb22d0dcfad6e1cfb429a1a579774585f50d
Vassilis Lambropoulos <lampropoulosv@kotsovolos.gr>
2019-09-04 13:49:05 +0300

    Add Spring Boot project generated on start.spring.io.

---
+++ src/main/java/org/lampropoul/jobsfeeder/JobsFeederApplication.java
@@ -0,0 +1,13 @@
+package org.lampropoul.jobsfeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobsFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobsFeederApplication.class, args);
+	}
+
+}
