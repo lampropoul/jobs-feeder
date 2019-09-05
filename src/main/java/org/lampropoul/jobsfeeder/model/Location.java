114df8624345f76fa1caacb366de16fb99c3e68a
Vassilis Lambropoulos <lampropoulosv@kotsovolos.gr>
2019-09-05 12:05:45 +0300

    Change Location object.

--- src/main/java/org/lampropoul/jobsfeeder/model/Location.java
+++ src/main/java/org/lampropoul/jobsfeeder/model/Location.java
@@ -2,6 +2,8 @@ package org.lampropoul.jobsfeeder.model;
 
 public class Location {
     private String country;
+    private String region;
     private String city;
-    private String suburb;
+    private String municipality;
+    private String postalCode;
 }
