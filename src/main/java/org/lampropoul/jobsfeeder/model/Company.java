2a474c1b6a845f7da3bdb40f533f428d739b8a13
Vassilis Lambropoulos <lampropoulosv@kotsovolos.gr>
2019-09-05 15:51:42 +0300

    Add spring-boot-starter-data-jpa dependency.

--- src/main/java/org/lampropoul/jobsfeeder/model/Company.java
+++ src/main/java/org/lampropoul/jobsfeeder/model/Company.java
@@ -2,7 +2,10 @@ package org.lampropoul.jobsfeeder.model;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+
 @Data
+@Entity
 public class Company extends BaseObject {
     private String name;
     private Location location;
--- src/main/java/org/lampropoul/jobsfeeder/model/Location.java
+++ src/main/java/org/lampropoul/jobsfeeder/model/Location.java
@@ -1,5 +1,11 @@
 package org.lampropoul.jobsfeeder.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+@Data
+@Entity
 public class Location {
     private String country;
     private String region;
