f670d6aa2f7e149cc7777eefcc7580b3243dac30
Vassilis Lambropoulos <lampropoulosv@kotsovolos.gr>
2019-09-18 12:49:31 +0300

    Add findAllByLocation in CompanyRepository.

--- src/main/java/org/lampropoul/jobsfeeder/repositories/CompanyRepository.java
+++ src/main/java/org/lampropoul/jobsfeeder/repositories/CompanyRepository.java
@@ -1,7 +1,11 @@
 package org.lampropoul.jobsfeeder.repositories;
 
 import org.lampropoul.jobsfeeder.model.Company;
+import org.lampropoul.jobsfeeder.model.Location;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CompanyRepository extends MongoRepository<Company, Long> {
+    List<Company> findAllByLocation(Location location);
 }
