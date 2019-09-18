7a34c89d1ed1713b344060f51ed165975cd18e67
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2019-09-18 12:39:50 +0300

    Update CompanyRepository.java

--- src/main/java/org/lampropoul/jobsfeeder/repositories/CompanyRepository.java
+++ src/main/java/org/lampropoul/jobsfeeder/repositories/CompanyRepository.java
@@ -3,5 +3,5 @@ package org.lampropoul.jobsfeeder.repositories;
 import org.lampropoul.jobsfeeder.model.Company;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface CompanyRepository extends MongoRepository<Long, Company> {
+public interface CompanyRepository extends MongoRepository<Company, Long> {
 }
