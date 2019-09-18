c546be18cbd1a0a60a85c20dc6a689f985c7f8c7
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2019-09-18 12:40:06 +0300

    Update LocationRepository.java

--- src/main/java/org/lampropoul/jobsfeeder/repositories/LocationRepository.java
+++ src/main/java/org/lampropoul/jobsfeeder/repositories/LocationRepository.java
@@ -3,5 +3,5 @@ package org.lampropoul.jobsfeeder.repositories;
 import org.lampropoul.jobsfeeder.model.Location;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface LocationRepository extends MongoRepository<Long, Location> {
+public interface LocationRepository extends MongoRepository<Location, Long> {
 }
