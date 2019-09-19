f24a113ce1ea7f538d13a78d48f775a930f00520
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2019-09-19 15:30:23 +0300

    Exclude DataSourceAutoConfiguration

--- src/main/java/org/lampropoul/jobsfeeder/JobsFeederApplication.java
+++ src/main/java/org/lampropoul/jobsfeeder/JobsFeederApplication.java
@@ -2,12 +2,13 @@ package org.lampropoul.jobsfeeder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class JobsFeederApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobsFeederApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobsFeederApplication.class, args);
+    }
 
 }
