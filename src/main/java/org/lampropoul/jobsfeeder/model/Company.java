a94976cc09b74632e10a666d5775a9b35964f045
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2019-09-22 21:56:52 +0300

    Change models.

--- src/main/java/org/lampropoul/jobsfeeder/model/Company.java
+++ src/main/java/org/lampropoul/jobsfeeder/model/Company.java
@@ -1,15 +1,19 @@
 package org.lampropoul.jobsfeeder.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Data
 @Document(collection = "companies")
 @EqualsAndHashCode(callSuper = false)
 public class Company extends BaseObject {
     private String name;
-    private Location location;
+    @JsonProperty("locations")
+    private List<Location> locationList;
     private String description;
     private short numOfJobs;
 }
--- src/main/java/org/lampropoul/jobsfeeder/model/Job.java
+++ src/main/java/org/lampropoul/jobsfeeder/model/Job.java
@@ -1,5 +1,6 @@
 package org.lampropoul.jobsfeeder.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,7 +12,12 @@ import java.util.List;
 @EqualsAndHashCode(callSuper = false)
 public class Job extends BaseObject {
     private String title;
+    private Company company;
+    private Location location;
+    @JsonProperty("jobDescriptions")
     private List<JobDescription> jobDescriptionList;
+    @JsonProperty("requirements")
     private List<Requirement> requirementList;
+    @JsonProperty("benefits")
     private List<Benefit> benefitList;
 }
