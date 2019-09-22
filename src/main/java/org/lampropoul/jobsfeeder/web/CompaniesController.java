2cef75c54c3ea672cd93827b673df576baf74078
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2019-09-22 17:42:05 +0300

    When creating a Company, create the Location if needed.

--- src/main/java/org/lampropoul/jobsfeeder/web/CompaniesController.java
+++ src/main/java/org/lampropoul/jobsfeeder/web/CompaniesController.java
@@ -2,21 +2,26 @@ package org.lampropoul.jobsfeeder.web;
 
 import org.lampropoul.jobsfeeder.model.BaseObject;
 import org.lampropoul.jobsfeeder.model.Company;
+import org.lampropoul.jobsfeeder.model.Location;
 import org.lampropoul.jobsfeeder.repositories.CompanyRepository;
+import org.lampropoul.jobsfeeder.repositories.LocationRepository;
 import org.lampropoul.jobsfeeder.services.SequenceGeneratorService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @RestController
 public class CompaniesController {
 
     private CompanyRepository companyRepository;
+    private LocationRepository locationRepository;
     private SequenceGeneratorService sequenceGeneratorService;
 
-    public CompaniesController(CompanyRepository companyRepository, SequenceGeneratorService sequenceGeneratorService) {
+    public CompaniesController(CompanyRepository companyRepository, LocationRepository locationRepository, SequenceGeneratorService sequenceGeneratorService) {
         this.companyRepository = companyRepository;
+        this.locationRepository = locationRepository;
         this.sequenceGeneratorService = sequenceGeneratorService;
     }
 
@@ -33,6 +38,14 @@ public class CompaniesController {
     @PostMapping("/companies/new")
     public Company create(@RequestBody Company company) {
         company.setId(sequenceGeneratorService.generateSequence(BaseObject.SEQUENCE_NAME));
+        Location location = company.getLocation();
+        if (location != null) {
+            Long locationId = (location.getId() != null) ? location.getId() : 0L;
+            if (!locationRepository.existsById(locationId)) {
+                location.setId(sequenceGeneratorService.generateSequence(BaseObject.SEQUENCE_NAME));
+                locationRepository.save(location);
+            }
+        }
         return companyRepository.save(company);
     }
 }
