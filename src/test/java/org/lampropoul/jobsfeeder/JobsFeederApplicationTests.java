16d1a8e63d1776233e3ab4323322e3b327e3577f
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2019-09-23 17:55:36 +0300

    Add integration tests.

--- src/test/java/org/lampropoul/jobsfeeder/JobsFeederApplicationTests.java
+++ src/test/java/org/lampropoul/jobsfeeder/JobsFeederApplicationTests.java
@@ -2,15 +2,36 @@ package org.lampropoul.jobsfeeder;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.lampropoul.jobsfeeder.model.Job;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class JobsFeederApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    private TestRestTemplate restTemplate = new TestRestTemplate();
+    private HttpHeaders headers = new HttpHeaders();
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void createJob() {
+        Job job = new Job();
+        job.setTitle("Backend Engineer");
+        HttpEntity<Job> entity = new HttpEntity<>(job, headers);
+        ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/jobs/new"), HttpMethod.POST, entity, String.class);
+        assert response != null;
+    }
+
+    private String createURLWithPort(String path) {
+        return "http://localhost:" + port + path;
+    }
 
 }
