43c73b31193116a6b711ff2a2675498aedf2ef49
Vassilis Lambropoulos <lampropoul1989@gmail.com>
2019-12-10 21:07:21 +0200

    Use JUnit 5 for tests

--- src/test/java/org/lampropoul/jobsfeeder/JobsFeederApplicationTests.java
+++ src/test/java/org/lampropoul/jobsfeeder/JobsFeederApplicationTests.java
@@ -1,8 +1,9 @@
 package org.lampropoul.jobsfeeder;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.lampropoul.jobsfeeder.model.Company;
 import org.lampropoul.jobsfeeder.model.Job;
 import org.lampropoul.jobsfeeder.model.Location;
@@ -10,37 +11,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class JobsFeederApplicationTests {
 
     private Job job;
-    private TestRestTemplate testRestTemplate;
+    private TestRestTemplate restTemplate;
     private HttpHeaders headers;
     @LocalServerPort
     private int port;
 
-    @Before
-    public void setUp() {
-        testRestTemplate = new TestRestTemplate();
+    JobsFeederApplicationTests() {
+        restTemplate = new TestRestTemplate();
         headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
         job = new Job();
         job.setTitle("Backend Engineer");
         job.setCompany(new Company());
         job.setLocation(new Location());
     }
 
-    @Test
-    public void createJob() {
-        HttpEntity<Job> entity = new HttpEntity<>(job, headers);
-        ResponseEntity<Job> response = testRestTemplate.exchange(createURLWithPort("/jobs"), HttpMethod.POST, entity, Job.class);
-        assert response.getStatusCode() == HttpStatus.CREATED || response.getStatusCode() == HttpStatus.OK;
-    }
-
     private String createURLWithPort(String path) {
         return "http://localhost:" + port + path;
+    }
+
+    @Test
+    @Order(1)
+    public void createJob() {
+        HttpEntity<Job> entity = new HttpEntity<>(job, headers);
+        ResponseEntity<Job> response = restTemplate.exchange(createURLWithPort("/jobs"), HttpMethod.POST, entity, Job.class);
+        assert response.getStatusCode() == HttpStatus.CREATED || response.getStatusCode() == HttpStatus.OK;
     }
 
 }
